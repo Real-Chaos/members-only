ffcda43950c81390566fe7335bfea6a72299b801
Rizwan Nafees <rizwannafees13@gmail.com>
2024-11-21 00:00:30 -0500

    added a script for deployment

--- app.js
+++ app.js
@@ -137,6 +137,6 @@ app.get('/logout', (req, res, next) => {
 	})
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listening to app on PORT ${PORT}`))
